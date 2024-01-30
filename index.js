0acc5793452222f46a7151677b33a21b5e256bcc
AV306 <66512356+AV306@users.noreply.github.com>
2024-01-30 08:39:29 +0000

    Ee

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+const aboutBox = document.getElementById( "about-box" );
+const projectsBox = document.getElementById( "projects-box" );
+const contactBox = document.getElementById( "contact-box" );
+
+const defaultClass = "main blur";
+
 function show( index )
 {
     switch ( index )
     {
         case 0:
+			aboutBox.class = defaultClass;
+			projectsBox.class += " hidden";
+			contactBox.class += " hidden";
             break;
 
         case 1:
+			aboutBox.class += " hidden";
+			projectsBox.class = defaultClass;
+			contactBox.class += " hidden";
+			alert( "test" )
             break;
 
         case 2:
+			aboutBox.class += " hidden";
+			projectsBox.class += " hidden";
+			contactBox.class = defaultClass;
             break;
 
         default:
